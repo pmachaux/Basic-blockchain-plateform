ea11adde8a077700a6c33b2e55386cdcdc44707f
Pierre <pierre.machaux@gmail.com>
2018-09-06 21:07:55 +0200

    implement difficulty principle

--- src/state/state-manager.ts
+++ src/state/state-manager.ts
@@ -1,40 +1,36 @@
 import {Block} from '../blockchain/models/block.model';
 import * as WebSocket from 'ws';
-import {Observable} from 'rxjs/internal/Observable';
 import {Subject} from 'rxjs/internal/Subject';
 
 const state: {
-    blockchain: Block[],
-    sockets: WebSocket[]
+  blockchain: Block[];
+  sockets: WebSocket[];
 } = {
-    blockchain: null,
-    sockets: []
+  blockchain: null,
+  sockets: [],
 };
 
 const blockchainSubject: Subject<Block[]> = new Subject<Block[]>();
 
 export class StateManager {
+  getSockets(): WebSocket[] {
+    return [...state.sockets];
+  }
 
-    getSockets(): WebSocket[] {
-        return [...state.sockets];
-    }
+  setSockets(data: WebSocket[]): void {
+    state.sockets = data;
+  }
 
-    setSockets(data: WebSocket[]): void {
-        state.sockets = data;
-    }
+  getBlockChain(): Block[] {
+    return state.blockchain ? state.blockchain.map(x => ({...x})) : null;
+  }
 
-    getBlockChain(): Block[]  {
-        return state.blockchain ? state.blockchain.map(x => ({...x})) : null;
-    }
+  setBlockChain(data: Block[]): void {
+    state.blockchain = data.sort((a, b) => a.index - b.index).map(x => ({...x}));
+    blockchainSubject.next(data);
+  }
 
-    setBlockChain(data: Block[]): void {
-        state.blockchain = data.sort(((a, b) => (a.index - b.index))).map(x => ({...x}));
-        blockchainSubject.next(data);
-    }
-
-    onBlockChainChange(): Subject<Block[]> {
-        return blockchainSubject;
-    }
+  onBlockChainChange(): Subject<Block[]> {
+    return blockchainSubject;
+  }
 }
-
-const stateManager = new StateManager();
